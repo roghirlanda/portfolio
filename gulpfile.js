7555e1d230866acdca4097dc5ce90cb7beb01251
Roberto Ghirlanda <roberto.g.biamino@gmail.com>
2018-05-28 17:28:15 -0300

    gulp e css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require( 'gulp' );
-var minifycss = require( 'gulp-minify-css' );
+var minifycss = require( 'gulp-clean-css' );
 var sass = require( 'gulp-sass' );
 var notify = require( 'gulp-notify' );
 
